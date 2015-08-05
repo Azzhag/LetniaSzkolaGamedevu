94d73944a310d28df4f4fb8c9a8bd11b55be6045
FeniXb3 <fenix.b3@gmail.com>
2015-08-05 23:13:04 +0200

    Added movement looping script

---
+++ Assets/MovementLooper.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(OneAxisMover))]
+public class MovementLooper : MonoBehaviour
+{
+    private OneAxisMover mover;
+
+    void Awake()
+    {
+        mover = GetComponent<OneAxisMover>();
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Finish")
+        {
+            var newPosition = transform.position;
+            switch (mover.moveAxis)
+            {
+                case AxisEnum.Horizontal:
+                    newPosition.x = -newPosition.x;
+                    break;
+                case AxisEnum.Vertical:
+                    newPosition.y = -newPosition.y;
+                    break;
+
+                default:
+                    break;
+            }
+            transform.position = newPosition;
+        }
+    }
+}
